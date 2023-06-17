f9f7cf736523b5ac0e0f74def5f8117862342eee
culpepperd <culpepperd2490@gmail.com>
2023-06-17 18:17:53 -0400

    added color to .html, and undefined examples to .js

--- typeConvPractice.js
+++ typeConvPractice.js
@@ -1,6 +1,28 @@
-// make function(s) to add new element to webpage and
-// print explanation and results of type conversions/computations
-// so not just visible in console
+function addConvData() {
 
-var addi = 3 + "4";
-console.log(addi);
+	// undefined examples
+	var und1 = document.getElementById("und1");
+	und1.innerHTML = "A function without a return statement will return: undefined";
+	turnTxtRed(und1);
+	var und2 = document.getElementById("und2");
+	und2.innerHTML = "The value of a variable without an assigned value is: undefined";
+	turnTxtRed(und2);
+	var und3 = document.getElementById("und3");
+	und3.innerHTML = "The value of an array item that doesn't exist in a sparse array: undefined";
+	turnTxtRed(und3);
+	var und4 = document.getElementById("und4");
+	und4.innerHTML = "The value of a property that doesn't exist: undefined";
+	turnTxtRed(und4);
+	var und5 = document.getElementById("und5");
+	und5.innerHTML = "The value of a property that's been deleted: undefined";
+	turnTxtRed(und5);
+	var und6 = document.getElementById("und6");
+	und6.innerHTML = "The value that can't be assigned to a property when an object is created: undefined";
+	turnTxtRed(und6);
+
+	// function to turn all undefined text examples red
+	function turnTxtRed(elementId) {
+		return elementId.setAttribute("class", "redtxt");
+	}
+}
+window.onload = addConvData;
