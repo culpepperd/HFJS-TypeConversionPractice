43954f3ef72aa971a3ed5958a5c6f3b2305490fd
culpepperd <culpepperd2490@gmail.com>
2023-06-17 11:08:12 -0400

    added 1 conversion to .js and comments for project goals

---
+++ typeConvPractice.js
@@ -0,0 +1,6 @@
+// make function(s) to add new element to webpage and
+// print explanation and results of type conversions/computations
+// so not just visible in console
+
+var addi = 3 + "4";
+console.log(addi);
