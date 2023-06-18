c8ee42b25f253a86b4cc3344461d22decb7cc391
culpepperd <culpepperd2490@gmail.com>
2023-06-17 21:07:50 -0400

    added typeof examples to .js and printed to webpage in .html

--- typeConvPractice.js
+++ typeConvPractice.js
@@ -1,6 +1,12 @@
 function addConvData() {
 
-	// undefined examples
+	printUndEx();
+	printTypeOf();
+}
+window.onload = addConvData;
+
+// function prints undefined examples
+function printUndEx() {
 	var und1 = document.getElementById("und1");
 	und1.innerHTML = "A function without a return statement will return: undefined";
 	turnTxtRed(und1);
@@ -19,10 +25,37 @@ function addConvData() {
 	var und6 = document.getElementById("und6");
 	und6.innerHTML = "The value that can't be assigned to a property when an object is created: undefined";
 	turnTxtRed(und6);
-
-	// function to turn all undefined text examples red
-	function turnTxtRed(elementId) {
-		return elementId.setAttribute("class", "redtxt");
-	}
 }
-window.onload = addConvData;
+
+// function to turn all undefined text examples red
+function turnTxtRed(elementId) {
+	return elementId.setAttribute("class", "redtxt");
+}
+
+function printTypeOf() {
+	var test1 = "abcdef";
+	var test2 = 123;
+	var test3 = true;
+	var test4 = {};
+	var test5 = [];
+	var test6;
+	var test7 = { "abcdef": 123 };
+	var test8 = ["abcdef", 123];
+
+	var typeOf1 = document.getElementById("typeOf1");
+	typeOf1.innerHTML = (typeof test1);
+	var typeOf2 = document.getElementById("typeOf2");
+	typeOf2.innerHTML = (typeof test2);
+	var typeOf3 = document.getElementById("typeOf3");
+	typeOf3.innerHTML = (typeof test3);
+	var typeOf4 = document.getElementById("typeOf4");
+	typeOf4.innerHTML = (typeof test4);
+	var typeOf5 = document.getElementById("typeOf5");
+	typeOf5.innerHTML = (typeof test5);
+	var typeOf6 = document.getElementById("typeOf6");
+	typeOf6.innerHTML = (typeof test6);
+	var typeOf7 = document.getElementById("typeOf7");
+	typeOf7.innerHTML = (typeof test7);
+	var typeOf8 = document.getElementById("typeOf8");
+	typeOf8.innerHTML = (typeof test8);
+}
